ec445b379926dc87fbfc2c257dcd94c9314bce1c
KrishanMohanDev <94595471+KrishanMohanDev@users.noreply.github.com>
2022-04-24 17:04:42 +0530

    Update users.sql

--- Login System/users.sql
+++ Login System/users.sql
@@ -18,7 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `users`
+create Database `users`;
+use `users`;
 --
 
 -- --------------------------------------------------------
